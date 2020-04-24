299fee86f492b964cb4d374c5b96436f924d220d
Flo <Florianb@payspace.com>
2020-04-24 10:58:29 +0200

    Fixed className attribute

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import burgerLogo from '../../assets/images/original.png';
 import classes from './Logo.module.css';
 
 const logo = (props) => (
-    <div class={classes.Logo}>
+    <div className={classes.Logo}>
         <img src={burgerLogo} alt="Burger Builder" />
     </div>
 );
