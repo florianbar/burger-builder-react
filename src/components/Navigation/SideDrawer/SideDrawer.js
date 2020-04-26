6cc3dcdf2635226b78bb59a72eaafae93226008d
florianbar <florianbar01@gmail.com>
2020-04-26 14:22:53 +0200

    Added logic to hide SideDrawer and Backdrop

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,18 +3,28 @@ import React from 'react';
 import classes from './SideDrawer.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
-    // ...
+    const attachedClasses = [
+        classes.SideDrawer, 
+        props.open ? classes.Open : classes.Close
+    ];
+
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <React.Fragment>
+            <Backdrop 
+                show={props.open} 
+                clicked={props.closed} />
+            <div className={attachedClasses.join(" ")}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </React.Fragment>
     );
 } 
 
