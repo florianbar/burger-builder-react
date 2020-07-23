07c0e6ec81f7956d2a9c6abb51b4e559752572ab
florianbar <florianbar01@gmail.com>
2020-07-23 15:33:45 +0200

    added order post with data

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -88,7 +89,29 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert("You continue");
+        //alert("You continue");
+        const orderData = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: "Flo Bär",
+                address: {
+                    street: "22 Cross Road",
+                    zipCode: "7441",
+                    country: "South Africa"
+                },
+                email: "test@test.com"
+            },
+            deliveryMethod: "fastest"
+        };
+
+        axios.post("/orders.json", orderData)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     render() {
