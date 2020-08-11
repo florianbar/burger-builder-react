66377c3b123d97f2a67fccfd85eda055b98ed4cd
florianbar <florianbar01@gmail.com>
2020-08-11 14:44:15 +0200

    Fetching Orders data

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
+import axios from '../../axios-orders';
 
 import Order from '../../components/Order/Order';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    };
+
+    componentDidMount () {
+        axios.get("/orders.json")
+            .then(response => {
+                const fetchedOrders = [];
+                for (let key in response.data) {
+                    fetchedOrders.push({
+                        id: key,
+                        ...response.data[key]
+                    });
+                }
+                this.setState({
+                    orders: fetchedOrders, 
+                    loading: false
+                });
+            })
+            .catch(error => {
+                console.log(error);
+                this.setState({loading: false});
+            });
+    }
+
     render () {
+        let orders = <Spinner />;
+
+        if (!this.state.loading) {
+            orders = (
+                <React.Fragment>
+                    <Order />
+                    <Order />
+                </React.Fragment>
+            );
+        }
+
         return (
             <div>
-                <Order />
-                <Order />
+                {orders}
             </div>
         );
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
